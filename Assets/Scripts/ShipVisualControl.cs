438d95d2e4befc55ab50414c6850ab12d7a22c0b
VPJP <vpjpeltonen@gmail.com>
2020-05-01 13:14:57 +0300

    basic control works

---
+++ Assets/Scripts/ShipVisualControl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipVisualControl : MonoBehaviour
+{
+    public Transform target, steeringBall;
+    private float turnSpeed = 0.05f; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //transform.LookAt(steeringBall);
+        transform.position = new Vector3(target.position.x, transform.position.y, target.position.z);
+
+        float turnInput = Input.GetAxis ("Horizontal");
+        //transform.position += -transform.right * turnInput * turnSpeed;
+        transform.Rotate(Vector3.up * turnInput);
+        //body.AddRelativeTorque(0f, turnInput * turnSpeed,0f);
+        //transform.rotation = target.rotation;
+       //transform.eulerAngles = new Vector3(transform.eulerAngles.x, target.eulerAngles.y, transform.eulerAngles.z);
+    }
+}
---
+++ Assets/Scripts/SteeringControl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SteeringControl : MonoBehaviour
+{
+    public Transform ship; 
+    private float turnSpeed = 0.05f; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //float y = transform.position.y;
+        
+        //float distance = 5.0f;
+        //transform.position = (transform.position - ship.transform.position).normalized * distance + ship.transform.position;
+        //float turnInput = Input.GetAxis ("Horizontal");
+        //transform.position += -transform.right * turnInput * turnSpeed; //new Vector3(transform.position.x+(turnInput*turnSpeed),transform.position.y,transform.position.z);
+
+        //transform.position = new Vector3(transform.position.x, y, transform.position.z);
+        //transform.LookAt(ship);
+    }
+}
